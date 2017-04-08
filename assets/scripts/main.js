f0422dfc534568bb66f2d1b23f510a70ae848551
joshin-run <jahmsc@gmail.com>
2017-04-08 17:02:52 -0600

    Feat (index) Add menu drop down for custom date/title

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -27,44 +27,104 @@ function countdown() {
 }
 countdown();
 
-function validateNumber(number) {
-  let msg = '';
-  if (number != null) {
-    if (isNaN(number)) {
-      msg = 'The input is not a valid number';
-    } else {
-      msg = (number %2 == 0) ? "Even Number" : "Odd Number";
-    }
-  } else {
-    msg = 'You cancelled the action';
+function popupTitle() {
+  let newTitle = prompt("What would you like to name your countdown clock?");
+  document.getElementById('clockTitle').innerHTML = newTitle;
+}
+
+function popupDate() {
+  let newDay = prompt("Choose a day of the month.");
+  let newMonth = prompt("Choose a month out of the year. (Use numbers)");
+  let newYear = prompt("Choose a year. (Use four digits)");
+  function newCountdown() {
+    let now = new Date()
+    let eventDate = new Date(newYear, newMonth, newDay);
+    let currentTime = now.getTime();
+    let eventTime = eventDate.getTime();
+
+    let remTime = eventTime - currentTime;
+
+    let s = Math.floor(remTime / 1000);
+    let m = Math.floor(s / 60);
+    let h = Math.floor(m / 60);
+    let d = Math.floor(h / 24);
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").innerText = d;
+    document.getElementById("hours").innerText = h;
+    document.getElementById("minutes").innerText = m;
+    document.getElementById("seconds").innerText = s;
+
+    setTimeout(countdown, 1000);
   }
-  document.getElementById('result').innerText = msg;
+  newCountdown();
 }
 
-let customPrompt = new function(msg, callback) {
-  this.show = function() {
-    let dlg = docuent.getElementById('dialog-container');
-    dlg.style.top = '30%';
-    dlg.style.opacity = 1;
-    let dlgMessage = dlg.querySelector('#dlg-message');
 
-  };
-}
-
-function userEventDate (year, month, day) {
-  let newEventDate = new Date(year, month, day);
-  return newEventDate;
-}
-
-msg1 = "Year?";
-msg2 = "Month?";
-msg3 = "Day?";
-title = "Name your countdown clock.";
-
-// <!-- I stopped listening around 5 min for JS 2/2 -->
-
-// function changeBackground() {
-//   body {
-//     background-image: url('...');
+//
+// function validateNumber(number) {
+//   let msg = '';
+//   if (number != null) {
+//     if (isNaN(number)) {
+//       msg = 'The input is not a valid number';
+//     } else {
+//       msg = (number %2 == 0) ? "Even Number" : "Odd Number";
+//     }
+//   } else {
+//     msg = 'You cancelled the action';
 //   }
+//   document.getElementById('result').innerText = msg;
 // }
+//
+// let customPrompt = new function(msg, callback) {
+//   this.show = function() {
+//     let dlg = docuent.getElementById('dialog-container');
+//     dlg.style.top = '30%';
+//     dlg.style.opacity = 1;
+//     let dlgMessage = dlg.querySelector('#dlg-message');
+//
+//   };
+// }
+//
+// function userEventDate (year, month, day) {
+//   let newEventDate = new Date(year, month, day);
+//   return newEventDate;
+// }
+//
+// msg1 = "Year?";
+// msg2 = "Month?";
+// msg3 = "Day?";
+// title = "Name your countdown clock.";
+
+/*==CUSTOM DATE POPUP==*/
+
+
+
+
+
+/*==DROP DOWN==*/
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
